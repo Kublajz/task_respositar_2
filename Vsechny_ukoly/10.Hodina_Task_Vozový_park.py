7b2832430c61e8cc478553b6db42a8750180a1b8
Kublajz <allien56@seznam.cz>
2024-12-24 15:53:21 +0100

    Q23

--- Vsechny_ukoly/10.Hodina_Task_Vozový_park.py
+++ Vsechny_ukoly/10.Hodina_Task_Vozový_park.py
@@ -1,4 +1,5 @@
-
+#Implement a class Car. Class fields should store the following: model, year of release, manufacturer,
+#engine capacity, color, price. Implement class methods for data input and output, provide access to individual fields through class methods.
 
 class Car:
 
